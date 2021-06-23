8fa8ab5859e9010913a32574c825adb036d6b124
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-23 23:57:19 +0000

    importanto sklearn.model_selection

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
-from IPython.display import display
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+
+
+#Importar tabela para analise
 tabela = pd.read_csv("advertising.csv")
+
+sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
+plt.show()
+
+
+sns.pairplot(tabela)
+plt.show()
