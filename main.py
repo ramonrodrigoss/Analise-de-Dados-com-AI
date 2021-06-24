f36b95abd3d62b7601e126c4cffc787bf50408c5
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-24 00:17:12 +0000

    Criei e treinei as AI para analisar as info dos graficos

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 #Importar tabela para analise
 tabela = pd.read_csv("advertising.csv")
 
 sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
-plt.show()
-
+#plt.show()
 
 sns.pairplot(tabela)
-plt.show()
+#plt.show()
+
+y = tabela["Vendas"]
+x = tabela.drop("Vendas", axis=1)
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
+
+#Criar as AI
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
+
+#treina a AI
+modelo_regressaolinear.fit(x_treino, y_treino)
+modelo_arvoredecisao.fit(x_treino, y_treino)
+
