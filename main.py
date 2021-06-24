5a08e8c911fa5733e75fe05563300ff84fe776ff
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-24 00:51:23 +0000

    Exibindo o grafico mostrando aonde é melhor investir o dinheiro de propaganda, baseado no treino da melhor AI

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
 
 #Importar tabela para analise
 tabela = pd.read_csv("advertising.csv")
 
-sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
-#plt.show()
-
-sns.pairplot(tabela)
+#sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
+#sns.pairplot(tabela)
 #plt.show()
 
 y = tabela["Vendas"]
@@ -23,7 +22,26 @@ x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
 modelo_regressaolinear = LinearRegression()
 modelo_arvoredecisao = RandomForestRegressor()
 
+count= 0
+while count < 5:
 #treina a AI
-modelo_regressaolinear.fit(x_treino, y_treino)
-modelo_arvoredecisao.fit(x_treino, y_treino)
+  modelo_regressaolinear.fit(x_treino, y_treino)
+  modelo_arvoredecisao.fit(x_treino, y_treino)
+  count+=1
 
+#criar as previsores
+previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
+
+#comparar modelos
+print(metrics.r2_score(y_teste, previsao_regressaolinear))
+print(metrics.r2_score(y_teste, previsao_arvoredecisao))
+
+
+#plt.figure(figsize=(15,5))
+#sns.lineplot(data=tabela_auxiliar)
+
+
+
+sns.barplot(x=x_treino.columns, y=modelo_arvoredecisao.feature_importances_)
+plt.show()
